ee8c5ca988294bdbb87e7f953a0454ce59dd05ab
Perry M <pgmerilo@gmail.com>
2022-12-19 16:27:34 +0800

    Added tailwind Added navbar and login page

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    prefix: 'tw-',
+    content: [
+        './Pages/**/*.cshtml',
+        './Views/**/*.cshtml'
+    ],
+    theme: {
+        extend: {
+            boxShadow: {
+                'dark': '2px 3px rgb(0 0 0 / 1);',
+            }
+        },
+    },
+    plugins: [],
+    important: true,
+}
