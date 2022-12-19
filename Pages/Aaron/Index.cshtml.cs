a7c78951b4f88de9334f0d9dd2b54371c49fd5a7
Perry M <pgmerilo@gmail.com>
2022-12-19 16:42:05 +0800

    Added folders for everyone

---
+++ Pages/Aaron/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spoonful.Pages.Aaron
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
