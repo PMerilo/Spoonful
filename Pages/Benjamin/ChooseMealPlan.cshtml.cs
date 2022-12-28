adf24f2c15ab80f552f078aa004d947a33213818
Benjamin <benjamintanchingjui@gmail.com>
2022-12-28 15:15:12 +0800

    changes to home page

---
+++ Pages/Benjamin/ChooseMealPlan.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Spoonful.Pages.Benjamin
+{
+    public class ChooseMealPlanModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
