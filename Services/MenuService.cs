27b0a2284cf0dee4edf1b5d687f91996f2b3faab
Benjamin <benjamintanchingjui@gmail.com>
2022-12-31 10:43:26 +0800

    Added Category Add New Models and Flash message

---
+++ Services/MenuService.cs
@@ -0,0 +1,41 @@
+﻿using Spoonful.Models;
+
+namespace Spoonful.Services
+{
+	public class MenuService
+	{
+	}
+
+	public class CategoryService
+	{
+		private readonly AuthDbContext _context;
+
+		public CategoryService(AuthDbContext context)
+		{
+			_context = context;
+		}
+
+		public List<Category> GetAll()
+		{
+			return _context.Category.OrderBy(m=>m.name).ToList();
+		}
+
+		public Category? GetCategoryByName(string name)
+		{
+            Category? category = _context.Category.FirstOrDefault(x => x.name.Equals(name));
+            return category;
+		}
+
+		public void AddCategory(Category category)
+		{
+			_context.Category.Add(category);
+			_context.SaveChanges();
+		}
+
+		public void UpdateCategory(Category category)
+		{
+			_context.Category.Update(category);
+			_context.SaveChanges();
+		}
+	}
+}
