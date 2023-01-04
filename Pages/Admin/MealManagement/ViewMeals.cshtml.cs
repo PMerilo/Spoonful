802f80f4e6225f0366af95e9156c15b99f2fd541
Benjamin <benjamintanchingjui@gmail.com>
2023-01-04 12:25:27 +0800

    Still Making Changes

--- Pages/Admin/MealManagement/ViewMeals.cshtml.cs
+++ Pages/Admin/MealManagement/ViewMeals.cshtml.cs
@@ -1,10 +1,20 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Spoonful.Models;
+using Spoonful.Services;
 
 namespace Spoonful.Pages.Admin.MealManagement
 {
+    
     public class ViewMealsModel : PageModel
     {
+        private readonly AuthDbContext _db;
+
+        private readonly MenuItemService _menuItemService;
+
+        public IEnumerable<MenuItem> MenuItems { get; set; }
+
+        public MenuItem MyMenuItem { get; set; }
         public void OnGet()
         {
         }
