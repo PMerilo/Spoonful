3068c66eb7c26aae5e32c78b5bcee4d5f0579df9
Reegan Goe <reegangoe@gmail.com>
2023-01-12 05:18:34 +0800

    adding files

---
+++ Models/Recipe.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Spoonful.Models
+{
+	public class Recipe
+	{
+		public int Id { get; set; }
+
+		[Required]
+		public string? RecipeName { get; set; }
+
+		[Required]
+		public string? RecipeDesc { get; set; }
+
+		[Required]
+		public string? TimeTaken { get; set; }
+		
+	}
+}
