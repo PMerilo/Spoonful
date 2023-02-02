17348a930de47bc5fbee219d91659b6a561d534e
Perry M <pgmerilo@gmail.com>
2023-02-02 22:32:54 +0800

    WIP

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,19 +1,52 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Spoonful.Models;
+using Spoonful.Services;
+using Stripe;
 
 namespace Spoonful.Pages;
 
 public class IndexModel : PageModel
 {
     private readonly ILogger<IndexModel> _logger;
+    private readonly UserManager<CustomerUser> _userManager;
+    private readonly CustomerUserService _customerUserService;
 
-    public IndexModel(ILogger<IndexModel> logger)
+    public IndexModel(ILogger<IndexModel> logger, UserManager<CustomerUser> userManager, CustomerUserService customerUserService)
     {
         _logger = logger;
+        _userManager = userManager;
+        _customerUserService = customerUserService;
     }
 
-    public void OnGet()
+    public async Task OnGet()
     {
+        CustomerUser root = await _userManager.FindByNameAsync("rootuser");
+        if (root == null)
+        {
+            root = new CustomerUser
+            {
+                UserName = "rootuser"
+            };
+            var result = await _userManager.CreateAsync(root, "Password@123");
+            if (result.Succeeded)
+            {
+                await _customerUserService.SetUserRoleAsync(root.UserName, Roles.Admin);
+                TempData["FlashMessage.Text"] = "Created account successfully";
+                TempData["FlashMessage.Type"] = "success";
+            }
 
+        };
+    }
+
+    public async Task<IActionResult> OnPostDeleteUserAsync(string name)
+    {
+        var user = await _userManager.FindByNameAsync(name);
+        if (user != null)
+        {
+            await _userManager.DeleteAsync(user);
+        }
+        return RedirectToPage();
     }
 }
