f6c0849b02616c8b8c1bce4012f43ef8f31cb51d
Benjamin <benjamintanchingjui@gmail.com>
2023-02-12 13:10:28 +0800

    Meal kit Management for admin complete

--- Pages/Admin/MealKitManagement/ViewCustomerBox.cshtml.cs
+++ Pages/Admin/MealKitManagement/ViewCustomerBox.cshtml.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using Spoonful.Models;
 using Spoonful.Services;
 
+
 namespace Spoonful.Pages.Admin.MealKitManagement
 {
     public class ViewCustomerBoxModel : PageModel
@@ -14,8 +15,9 @@ namespace Spoonful.Pages.Admin.MealKitManagement
         private readonly MealOrderService _mealOrderService;
         private readonly MenuItemService _menuItemService;
         private readonly UserManager<CustomerUser> _userManager;
+        private readonly InvoiceMealKitService _invoiceMealKitService;
 
-        public ViewCustomerBoxModel(AuthDbContext db, OrderService orderService, MealKitService mealKitService, MealOrderService mealOrderService, MenuItemService menuItemService, UserManager<CustomerUser> userManager)
+        public ViewCustomerBoxModel(AuthDbContext db, OrderService orderService, MealKitService mealKitService, MealOrderService mealOrderService, MenuItemService menuItemService, UserManager<CustomerUser> userManager, InvoiceMealKitService invoiceMealKitService)
         {
             _db = db;
             _orderService = orderService;
@@ -23,6 +25,7 @@ namespace Spoonful.Pages.Admin.MealKitManagement
             _mealOrderService = mealOrderService;
             _menuItemService = menuItemService;
             _userManager = userManager;
+            _invoiceMealKitService = invoiceMealKitService;
         }
 
         public IEnumerable<Order> Orders { get; set; }
@@ -31,8 +34,24 @@ namespace Spoonful.Pages.Admin.MealKitManagement
         public Invoice MyInvoice { get; set; }
 
         public OrderDetails MyOrderDetails { get; set; }
-        public async Task<IActionResult> OnGet()
+        public async Task<IActionResult> OnGet(string id)
         {
+            MealKit? mealkit = _mealKitService.GetMealKitByUserId(id);
+            OrderDetails? orderDetails = _orderService.GetOrderDetailsByUserId(id);
+
+            Orders = _db.Order;
+            Orders = Orders.Where(X => X.OwnerID == id);
+
+            if (mealkit == null || orderDetails == null)
+            {
+                TempData["FlashMessage.Type"] = "danger";
+                TempData["FlashMessage.Text"] = ($"Customer not found");
+                return Redirect("/Admin/MealKitManagement/Index");
+            }
+            Invoice invoice = _invoiceMealKitService.GetInvoiceByMealKitId(mealkit.Id);
+            MyMealKit = mealkit;
+            MyInvoice = invoice;
+            MyOrderDetails = orderDetails;
             return Page();
         }
     }
