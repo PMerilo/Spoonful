ba29e491e34bc3408da0add07c341f9268881409
Revanth <revanth1509@gmail.com>
2023-02-12 23:26:10 +0800

    First working prototype of delivery tracking

--- Pages/Admin/DeliveryManagement/Index.cshtml.cs
+++ Pages/Admin/DeliveryManagement/Index.cshtml.cs
@@ -1,12 +1,51 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Spoonful.Models;
+using Spoonful.Services;
+using System.Text.RegularExpressions;
 
 namespace Spoonful.Pages.Admin.DeliveryManagement
 {
     public class IndexModel : PageModel
     {
+        private readonly MealKitService _mealKitService;
+        private readonly OrderService _orderService;
+        private readonly DeliveryService _deliveryService;
+
+        public IndexModel(AuthDbContext db, MealKitService mealKitService, OrderService orderService, DeliveryService deliveryService)
+        {
+            _mealKitService = mealKitService;
+            _orderService = orderService;
+            _deliveryService = deliveryService;
+        }
+
+        public List<Routes> routeList { get; set; } = new();
+
+        public List<Delivery> deliveryList { get; set; } = new();
         public void OnGet()
         {
+            deliveryList = _deliveryService.GetAllDeliveriesWithIncludes();
+        }
+
+        public async Task<JsonResult> OnPostCreateDeliveryRoutes(string route)
+        {
+            //find postal code in address
+            //string address = "115C Yishun Ring Road, Singapore 763115";
+            //string pattern = @"\b\d{6}\b";
+            //Match match = Regex.Match(address, pattern);
+            //if (match.Success)
+            //{
+            //    Console.WriteLine("Postal code: " + match.Value);
+            //}
+
+            //OrderDetails deets = _deliveryService.GetOrderDetailsbyPostalCode("763115");
+            //Console.WriteLine(deets.Address);
+
+            List<string> postalCodes = _deliveryService.GetOrderDetailsfortheDay();
+            _deliveryService.CreateRoutes(postalCodes);
+            //List<string> Sroute = new();
+            Console.WriteLine(route);
+            return new JsonResult(new { status = "Completed" });
         }
     }
 }
--- wwwroot/js/Routing.js
+++ wwwroot/js/Routing.js
@@ -1,18 +1,122 @@
-﻿
-// Initialize and add the map
+﻿$('#createRoutes').click(function () {
+    var route = "Central"
+    console.log(route)
+    $.ajax({
+        type: "POST",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val())
+        },
+        dataType: "json",
+        data: { route: route },
+        url: "/Admin/DeliveryManagement/?handler=CreateDeliveryRoutes",
+        success: function (data) {
+            console.log(data)
+            console.log("Routes created")
+        }
+    })
+})
+
+
+
+$('#test').click(function () {
+    var route = "North"
+    console.log(route)
+    $.ajax({
+        type: "POST",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader("XSRF-TOKEN", $('input:hidden[name="__RequestVerificationToken"]').val())
+        },
+        dataType: "json",
+        data: { route: route },
+        url: "/Admin/DeliveryManagement/DeliveryRouting?handler=RetrieveRoutes",
+        success: function (data) {
+            console.log(data.list)
+            var list = data.list
+            var last = list[list.length - 1] + ", singapore"
+            list.pop();
+            console.log(last)
+            console.log(list)
+            const directionsService = new google.maps.DirectionsService();
+            const directionsRenderer = new google.maps.DirectionsRenderer();
+            const map = new google.maps.Map(document.getElementById("map"), {
+                zoom: 11,
+                center: { lat: 1.3742002339365351, lng: 103.81540409330088 },
+            });
+            directionsRenderer.setMap(map);
+
+            const waypts = [];
+
+            for (let i = 0; i < list.length; i++) {
+                waypts.push({
+                    location: list[i] + ", singapore",
+                    stopover: true,
+                });
+            }
+
+            console.log(waypts)
+
+            directionsService
+                .route({
+                    origin: "768442, singapore",
+                    destination: last,
+                    waypoints: waypts,
+                    optimizeWaypoints: true,
+                    travelMode: google.maps.TravelMode.DRIVING,
+                })
+                .then((response) => {
+                    directionsRenderer.setDirections(response);
+                })
+                .catch((e) => window.alert("Directions request failed due to " + status));
+        }
+    })
+})
 function initMap() {
-    // The location of Uluru
-    const uluru = { lat: -25.344, lng: 131.031 };
-    // The map, centered at Uluru
+    const directionsService = new google.maps.DirectionsService();
+    const directionsRenderer = new google.maps.DirectionsRenderer();
     const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 4,
-        center: uluru,
+        zoom: 11,
+        center: { lat: 1.3742002339365351, lng: 103.81540409330088 },
     });
-    // The marker, positioned at Uluru
-    const marker = new google.maps.Marker({
-        position: uluru,
-        map: map,
+    directionsRenderer.setMap(map);
+
+    const onChangeHandler = function () {
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
+    };
+    document.getElementById("submit").addEventListener("click", () => {
+        calculateAndDisplayRoute(directionsService, directionsRenderer);
     });
+
+    document.getElementById("start").addEventListener("change", onChangeHandler);
+    document.getElementById("end").addEventListener("change", onChangeHandler);
 }
+
+function calculateAndDisplayRoute(directionsService, directionsRenderer) {
+    const waypts = [];
+    console.log(waypts)
+    const checkboxArray = document.getElementById("waypoints");
+
+    for (let i = 0; i < checkboxArray.length; i++) {
+        if (checkboxArray.options[i].selected) {
+            waypts.push({
+                location: checkboxArray[i].value,
+                stopover: true,
+            });
+        }
+    }
+
+    directionsService
+        .route({
+            origin: "763115",
+            destination: "310145",
+            waypoints: waypts,
+            optimizeWaypoints: true,
+            travelMode: google.maps.TravelMode.DRIVING,
+        })
+        .then((response) => {
+            directionsRenderer.setDirections(response);
+        })
+        .catch((e) => window.alert("Directions request failed due to " + status));
+}
+
 
 window.initMap = initMap;
